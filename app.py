8a139991837855f4fe76b3f6489e9134b63739bf
Ashok kumar <mani120488@gmail.com>
2021-09-28 20:39:49 +0530

    added changes to get form data and send as buffer
    added changes to get form data and send as buffer

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
 from flask import Flask,request, jsonify
+import pdfkit 
+import base64
+import os
 app = Flask(__name__)
 
-@app.route('/',methods=['POST' ])
-def hello():
-    import pdfkit 
+# @app.route('/host',methods=['POST' ])
+# def hello():
+#     htmlstr = request.form["html"]
+#     pdfkit.from_string(htmlstr, 'sample.pdf') 
+#     file = open("sample.pdf", "r")
+#     data = file.read()
+#     data = base64.encodebytes(data) 
+#     return jsonify({'data':data})
+
+
+@app.route('/pdf',methods=['POST' ])
+def pdf_convertor():
     htmlstr = request.form["html"]
-    print(htmlstr)
-    pdfkit.from_string(htmlstr, 'sample.pdf') 
-    #file = open("sample.pdf", "r")
-    #data = file.read()
-    #data = base64.encodebytes(data) 
-    #pdfkit.from_url('https://www.google.com/','sample.pdf')
-    #return jsonify({'data':data})
-    return "Done"
+
+    path_kit = r'/usr/local/bin/wkhtmltopdf'
+    config = pdfkit.configuration(wkhtmltopdf=path_kit)
+    pdfkit.from_string(htmlstr, 'sample.pdf',configuration=config) 
+
+    #pdfkit.from_string(htmlstr, 'sample.pdf')
+    data = open("sample.pdf", "rb").read()
+    encoded = base64.b64encode(data)
+    if os.path.exists("sample.pdf"):
+            os.remove("sample.pdf")
+    return jsonify({'encoded':str(encoded),'data':str(data)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000,debug=True)
