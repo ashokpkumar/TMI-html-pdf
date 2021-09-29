6f11d5e8bfa7d5060d5e6058c2a15dea6e3016fb
Ashok kumar <mani120488@gmail.com>
2021-09-29 17:41:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,20 +17,24 @@ app = Flask(__name__)
 
 @app.route('/pdf',methods=['POST' ])
 def pdf_convertor():
-    data = request.get_json()
-    htmlstr = data.get('html', None)
-    #htmlstr = request.form["html"]
+    # data = request.get_json()
+    # htmlstr = data.get('html', None)
+    htmlstr = request.form["html"]
     #json.decoder
-    # path_kit = r'/usr/local/bin/wkhtmltopdf'
-    # config = pdfkit.configuration(wkhtmltopdf=path_kit)
-    # pdfkit.from_string(htmlstr, 'sample.pdf',configuration=config) 
+    path_kit = r'/usr/local/bin/wkhtmltopdf'
+    config = pdfkit.configuration(wkhtmltopdf=path_kit)
+    pdfkit.from_string(htmlstr, 'sample.pdf',configuration=config) 
 
-    pdfkit.from_string(htmlstr, 'sample.pdf')
+    #pdfkit.from_string(htmlstr, 'sample.pdf')
     data = open("sample.pdf", "rb").read()
     encoded = base64.b64encode(data)
+    encoded = str(encoded)
+    encoded= encoded[2:]
+    encoded= encoded[:-1]
+    #print((encoded))
     if os.path.exists("sample.pdf"):
             os.remove("sample.pdf")
-    return jsonify({'encoded':str(encoded),'data':str(data)})
+    return jsonify({'data':str(encoded)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000,debug=True)
