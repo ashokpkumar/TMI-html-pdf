73d51d3e2dfecc9de285a54c2373cf9340420546
Ashok kumar <mani120488@gmail.com>
2021-10-01 14:50:23 +0530

    added waitress-code cleanup
    added waitress-code cleanup

--- app.py
+++ app.py
@@ -3,38 +3,26 @@ import pdfkit
 import base64
 import os
 import json
+from waitress import serve
+
 app = Flask(__name__)
-
-# @app.route('/host',methods=['POST' ])
-# def hello():
-#     htmlstr = request.form["html"]
-#     pdfkit.from_string(htmlstr, 'sample.pdf') 
-#     file = open("sample.pdf", "r")
-#     data = file.read()
-#     data = base64.encodebytes(data) 
-#     return jsonify({'data':data})
-
 
 @app.route('/pdf',methods=['POST' ])
 def pdf_convertor():
-    # data = request.get_json()
-    # htmlstr = data.get('html', None)
-    htmlstr = request.form["html"]
-    #json.decoder
+    data = request.get_json()
+    htmlstr = data.get('html', None)
     path_kit = r'/usr/local/bin/wkhtmltopdf'
     config = pdfkit.configuration(wkhtmltopdf=path_kit)
     pdfkit.from_string(htmlstr, 'sample.pdf',configuration=config) 
-
-    #pdfkit.from_string(htmlstr, 'sample.pdf')
     data = open("sample.pdf", "rb").read()
     encoded = base64.b64encode(data)
     encoded = str(encoded)
     encoded= encoded[2:]
     encoded= encoded[:-1]
-    #print((encoded))
     if os.path.exists("sample.pdf"):
             os.remove("sample.pdf")
     return jsonify({'data':str(encoded)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    serve(app, host='0.0.0.0', port=5000)
+    #app.run(host='0.0.0.0', port=5000,debug=True)
