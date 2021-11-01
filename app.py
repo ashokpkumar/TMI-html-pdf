7171e8f1c01e5f86534e4e0513f107da5e0f5daa
Ashok kumar <mani120488@gmail.com>
2021-11-01 09:35:24 +0530

    some changes

--- app.py
+++ app.py
@@ -10,19 +10,21 @@ app = Flask(__name__)
 @app.route('/pdf',methods=['POST' ])
 def pdf_convertor():
     data = request.get_json()
+    print(data)
     htmlstr = data.get('html', None)
-    path_kit = r'/usr/local/bin/wkhtmltopdf'
-    config = pdfkit.configuration(wkhtmltopdf=path_kit)
-    pdfkit.from_string(htmlstr, 'sample.pdf',configuration=config) 
+    #path_kit = r'/usr/local/bin/wkhtmltopdf'
+    #config = pdfkit.configuration(wkhtmltopdf=path_kit)
+    pdfkit.from_string(htmlstr, 'sample.pdf') 
+    #pdfkit.from_string(htmlstr, 'sample.pdf',configuration=config) 
     data = open("sample.pdf", "rb").read()
     encoded = base64.b64encode(data)
     encoded = str(encoded)
     encoded= encoded[2:]
     encoded= encoded[:-1]
-    if os.path.exists("sample.pdf"):
-            os.remove("sample.pdf")
+    # if os.path.exists("sample.pdf"):
+    #         os.remove("sample.pdf")
     return jsonify({'data':str(encoded)})
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=5000)
-    #app.run(host='0.0.0.0', port=5000,debug=True)
+    #serve(app, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000,debug=True)
